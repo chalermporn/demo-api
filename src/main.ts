12295579f63f9aef4b4d5bbc62dfec5576c3a360
codemunha <codemunha@gmail.com>
2020-09-13 23:48:24 +0700

    update swagger and config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  const port = 5000
+  const options = new DocumentBuilder()
+    .setTitle('product')
+    .setDescription('The Product API description')
+    .setVersion('1.0')
+    // .addTag('product')
+    .build()
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('api', app, document)
+
+  const port = 3000
   await app.listen(port)
   console.log('Server on port http://localhost:' + port)
 }
