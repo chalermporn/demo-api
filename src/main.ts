3a7922103413fd850ff8d10a7b9d8cf9c073e86e
codemunha <codemunha@gmail.com>
2020-09-13 23:21:26 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const port = 5000
   await app.listen(port)
-  console.log('Server on port http://localhost:'+ port)
+  console.log('Server on port http://localhost:' + port)
 }
 bootstrap()
--- src/product/dto/product.dto.ts
+++ src/product/dto/product.dto.ts
@@ -1,6 +1,6 @@
 export class CreateProductDTO {
     readonly name: string
-    readonly description:string
+    readonly description: string
     readonly imageURL: string
     readonly price: number
     readonly createdAt: Date
